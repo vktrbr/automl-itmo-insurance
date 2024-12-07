c9764c55d01b6ca98d9eb460bdd422cb5fe981c9
Victor Barbarich <victor.barbarich@dif.tech>
2024-12-07 23:38:53 +0100

    getting kaggle dataset

---
+++ src/config.py
@@ -0,0 +1,37 @@
+import os
+from pathlib import Path
+
+from dotenv import load_dotenv
+from loguru import logger
+
+# Load environment variables from .env file if it exists
+load_dotenv()
+
+# Paths
+PROJ_ROOT = Path(__file__).resolve().parents[1]
+logger.info(f"PROJ_ROOT path is: {PROJ_ROOT}")
+
+DATA_DIR = PROJ_ROOT / "data"
+RAW_DATA_DIR = DATA_DIR / "raw"
+INTERIM_DATA_DIR = DATA_DIR / "interim"
+PROCESSED_DATA_DIR = DATA_DIR / "processed"
+EXTERNAL_DATA_DIR = DATA_DIR / "external"
+
+MODELS_DIR = PROJ_ROOT / "models"
+
+REPORTS_DIR = PROJ_ROOT / "reports"
+FIGURES_DIR = REPORTS_DIR / "figures"
+
+KAGGLE_DATASET_NAME = "playground-series-s4e12"
+KAGGLE_CONFIG_DIR = PROJ_ROOT / ".kaggle"
+os.environ["KAGGLE_CONFIG_DIR"] = str(KAGGLE_CONFIG_DIR)
+
+# If tqdm is installed, configure loguru with tqdm.write
+# https://github.com/Delgan/loguru/issues/135
+try:
+    from tqdm import tqdm
+
+    logger.remove(0)
+    logger.add(lambda msg: tqdm.write(msg, end=""), colorize=True)
+except ModuleNotFoundError:
+    pass
---
+++ src/dataset.py
@@ -0,0 +1,130 @@
+import os
+import zipfile
+
+import typer
+from loguru import logger
+
+from config import (
+    EXTERNAL_DATA_DIR, KAGGLE_DATASET_NAME, RAW_DATA_DIR
+)
+
+app = typer.Typer(pretty_exceptions_enable=False)
+
+
+@app.command()
+def main(
+        destination: str = typer.Option(
+            EXTERNAL_DATA_DIR,
+            help="The destination folder to download the dataset"
+        ),
+        dataset: str = typer.Option(
+            KAGGLE_DATASET_NAME, help="The dataset name to download"
+        ),
+        reload_if_exists: bool = typer.Option(
+            False, help="If the dataset already exists, reload it"
+        )
+):
+    """
+    Download the dataset from Kaggle
+
+    . code-block:: bash
+        dvc run -n download_dataset -d dataset.py -o data/raw python dataset.py
+
+
+    :param destination: The destination folder
+    :param dataset: The dataset name. We are using the Insurance Regression DS
+    :param reload_if_exists: If the dataset already exists, reload it
+    :return:
+    """
+
+    # Authenticate to Kaggle API
+    authenticate()
+
+    # Download the dataset
+    path_to_dataset = get_data(destination, dataset, reload_if_exists)
+
+    # Unzip the dataset
+    files = unzip_data(path_to_dataset)
+
+    return files
+
+
+def authenticate():
+    """
+    Authenticate the user to Kaggle API
+    """
+    logger.info(os.environ.get('KAGGLE_CONFIG_DIR'))
+    import kaggle.api
+
+    # you should have a kaggle.json file in your home directory
+
+    kaggle.api.authenticate()
+    logger.info("Authenticated to Kaggle API")
+
+
+def get_data(
+        destination: str = EXTERNAL_DATA_DIR,
+        dataset: str = KAGGLE_DATASET_NAME,
+        reload_if_exists: bool = False
+) -> str:
+    """
+    Download the dataset from Kaggle
+
+    :param destination: The destination folder
+    :param dataset: The dataset name. We are using the Insurance Regression DS
+    :param reload_if_exists: If the dataset already exists, reload it
+    :return: The path to the downloaded dataset
+    """
+
+    destination = os.path.join(destination, f"{dataset}.zip")
+
+    if not reload_if_exists and os.path.exists(destination):
+        logger.info(f"Dataset already exists at {destination}")
+        return destination
+
+    import kaggle.api
+
+    # Download the dataset
+    logger.info(f"Downloading dataset {dataset} to {destination}")
+
+    result = kaggle.api.competitions_data_download_files(
+        dataset,
+        _preload_content=False
+    )
+
+    with open(destination, 'wb') as f:
+        f.write(result.data)
+
+    logger.success(f"SUCCESS: Downloaded dataset {dataset} to {destination}")
+
+    return destination
+
+
+def unzip_data(
+        path_to_dataset: str,
+        target_directory: str = RAW_DATA_DIR
+) -> list:
+    """
+    Unzip the dataset
+
+    :param path_to_dataset:
+    :param target_directory:
+    :return:
+    """
+
+    logger.info(f"Unzipping dataset {path_to_dataset} to {target_directory}")
+
+    with zipfile.ZipFile(path_to_dataset, 'r') as zip_ref:
+        zip_ref.extractall(target_directory)
+
+    logger.success(
+        f"SUCCESS: Unzipped dataset {path_to_dataset} to {target_directory}"
+    )
+
+    files = os.listdir(target_directory)
+
+    return files
+
+
+if __name__ == "__main__":
+    app()
