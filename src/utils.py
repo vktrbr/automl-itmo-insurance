cb87a14546b6b4478a517ff18a687e7aefe760d7
Victor Barbarich <victor.barbarich@dif.tech>
2024-12-08 15:47:26 +0100

    add preprocessing using rename columns and save to feather format

---
+++ src/utils.py
@@ -0,0 +1,11 @@
+import time
+
+
+class Timer:
+    def __enter__(self):
+        self.start = time.perf_counter()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.end = time.perf_counter()
+        self.elapsed = self.end - self.start
